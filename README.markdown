57ef14a79220ef234a87bd136b14b1fe5e527e7d
Eric Leclerc <eric.leclerc@gmail.com>
2009-04-29 21:13:46 -0400

    basic environment setup
    - view
    - layout
    - db
    - Scienta ZF Debug Bar

--- README.markdown
+++ README.markdown
@@ -1,4 +1,13 @@
-# SubYT
+SubYT
+=====
 
 SubYT is a niche video website engine. It's current purpose is to be a testing bed for GitHub, Zend Framework and gdata.
 
+
+Requirements
+------------
+
+These libraries are needed in the `library` folder
+
+- [Zend Framewok](http://framework.zend.com) v1.8.0+
+- [Scienta ZF Debug Bar](http://jokke.dk/software/scientadebugbar) v1.4+
--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,48 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initView()
+    {
+        // Initialise view
+        $view = new Zend_View();
+        $view->doctype('XHTML1_TRANSITIONAL');
+        $view->headTitle('SubYT');
 
+        // Add it to the ViewRenderer
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+            'ViewRenderer'
+        );
+        $viewRenderer->setView($view);
 
+        // Return it, so that it can be stored by the bootstrap
+        return $view;
+    }
+
+    /**
+     * Initialize the Scienta ZF Debug Bar
+     */
+    protected function _initScienta()
+    {
+        $scConfig = $this->getOption('scienta');
+
+        if ($scConfig['enabled'] != 1) {
+            return;
+        }
+
+        require_once 'Scienta/Controller/Plugin/Debug.php';
+
+        // Ensure DB instance is present, and fetch it
+        $this->bootstrap('Db');
+        $db = $this->getResource('Db');
+
+        $options = array('database_adapter' => $db);
+        $scBar = new Scienta_Controller_Plugin_Debug($options);
+
+        // Ensure front controller instance is present, and fetch it
+        $this->bootstrap('FrontController');
+        $front = $this->getResource('FrontController');
+
+        $front->registerPlugin($scBar);
+    }
 }
 
