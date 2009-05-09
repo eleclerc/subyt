315919dd1c8e0a286eceabcc39599a150e9372e2
Eric Leclerc <eric.leclerc@gmail.com>
2009-05-09 15:42:58 -0400

    schema change

--- application/data/schema.sql
+++ application/data/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "video" (
-    "id" integer NOT NULL PRIMARY KEY,
+    "id" integer PRIMARY KEY  NOT NULL,
     "url" varchar(255) NOT NULL,
     "youtube_id" varchar(20) NOT NULL,
     "title" varchar(200) NOT NULL,
-    "published" bool NOT NULL,
-    "created_at" date NOT NULL,
-    "updated_at" date NOT NULL
+    "published" integer DEFAULT 0,
+    "created_at" datetime NOT NULL,
+    "updated_at" datetime NOT NULL
 );
 
 CREATE TABLE "tagcategory" (
@@ -18,8 +18,8 @@ CREATE TABLE "tag" (
     "video_id" integer NOT NULL REFERENCES "video" ("id"),
     "tag" varchar(64) NOT NULL,
     "tagcategory_id" integer NOT NULL REFERENCES "tagcategory" ("id"),
-    "created_at" date NOT NULL,
-    "updated_at" date NOT NULL
+    "created_at" datetime NOT NULL,
+    "updated_at" datetime NOT NULL
 );
 CREATE INDEX "tag_video_id" ON "tag" ("video_id");
 CREATE INDEX "tag_tagcategory_id" ON "tag" ("tagcategory_id");
--- application/models/DbTable/Tag.php
+++ application/models/DbTable/Tag.php
@@ -12,4 +12,26 @@ class Model_DbTable_Tag extends Zend_Db_Table_Abstract
             'refColumns'        => 'id'
         ),
     );
+    
+    public function insert(array $data)
+    {
+        if (!isset($data['created_at'])) {
+            $data['created_at'] = date('Y-m-d H:i:s');
+        }
+        
+        if (!isset($data['updated_at'])) {
+            $data['updated_at'] = date('Y-m-d H:i:s');
+        }
+        
+        return parent::insert($data);
+    }
+    
+    public function update(array $data, $where)
+    {
+        if (!isset($data['updated_at'])) {
+            $data['updated_at'] = date('Y-m-d H:i:s');
+        }
+        
+        return parent::update($data, $where);
+    }
 }
